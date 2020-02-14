97069d8b763cc7cb2263e96423e1b6bdce738ba2
francesco <info.francescobianco@gmail.com>
2020-02-14 12:04:21 +0100

    stash changes before fetch upstream

--- fetch-upstream.sh
+++ fetch-upstream.sh
@@ -4,16 +4,22 @@ set -e
 git add .
 git commit -am "stash changes before fetch upstream" && true
 
-git checkout hotfix
-git pull https://github.com/Myddleware/myddleware.git hotfix
-git push
+#git checkout hotfix
+#git pull https://github.com/Myddleware/myddleware.git hotfix
+#git push
 
-git checkout master
-git pull https://github.com/Myddleware/myddleware.git master
-git push
+#git checkout master
+#git pull https://github.com/Myddleware/myddleware.git master
+#git push
 
-git checkout contribute
-git pull https://github.com/Myddleware/myddleware.git master
-git push
+#git checkout contribute
+#git pull https://github.com/Myddleware/myddleware.git master
+#git push
 
-git checkout develop
+git remote add upstream https://github.com/Myddleware/myddleware.git
+git fetch upstream
+git merge upstream/Release-2.5.2
+git push
+git remote rm upstream
+
+#git checkout develop
